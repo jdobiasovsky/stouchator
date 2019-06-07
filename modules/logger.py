52146ca14df9f1af65fe361ba7ee4bf2ad11eb53
Jan Dobiasovsky <dobiasovsky.honza@gmail.com>
2019-06-07 13:22:11 +0200

    Minor formatting

--- modules/logger.py
+++ modules/logger.py
@@ -6,8 +6,8 @@ import sys
 
 def setup_custom_logger(name):
     """Set up for app logging."""
-    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
-                                  datefmt='%Y-%m-%d %H:%M:%S')
+    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s \
+                                  %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     handler = logging.FileHandler('stouchator.log', mode='w')
     handler.setFormatter(formatter)
     screen_handler = logging.StreamHandler(stream=sys.stdout)
